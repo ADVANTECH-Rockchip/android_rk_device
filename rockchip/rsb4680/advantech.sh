bfc3c2ecb6b8349e45366614a1ca933278eec597
zengwei18488 <wei.zeng@advantech.com.cn>
2017-09-14 14:46:26 +0800

    add rsb4680

---
+++ rockchip/rsb4680/advantech.sh
@@ -0,0 +1,35 @@
+#!/system/bin/sh
+
+insmod /system/lib/module/bcmdhd.ko firmware_path=/system/etc/firmware/43241b4.bin nvram_path=/system/etc/firmware/bcm943241ng1630_p252.txt iface_name=wlan0
+
+logFile=/data/bootCount.tt
+adv_hctosys_sleep_time=7200
+
+
+bootCount()
+{
+
+	if [ -e ${logFile} ] ; then 
+		logNu=`cat ${logFile}`
+		countNu=$((${logNu}+1))
+		echo ${countNu} > ${logFile}
+	else
+		touch ${logFile}
+		echo "1" > ${logFile}
+	fi
+}
+
+adv_hctosys()
+{
+	echo "adv_hctosys -- sync every " ${adv_hctosys_sleep_time} " seconds"
+
+	while :
+	do
+		sleep ${adv_hctosys_sleep_time}
+		busybox hwclock -s -u
+	done
+}
+
+
+bootCount
+adv_hctosys
