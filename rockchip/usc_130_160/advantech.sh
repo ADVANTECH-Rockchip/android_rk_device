d40f2a622aa833f94100c0f031bed829423af48f
zengwei18488 <wei.zeng@advantech.com.cn>
2018-09-19 17:14:30 +0800

    added project usc_130_160

---
+++ rockchip/usc_130_160/advantech.sh
@@ -0,0 +1,40 @@
+#!/system/bin/sh
+
+insmod /system/lib/module/bcmdhd.ko firmware_path=/system/etc/firmware/43241b4.bin nvram_path=/system/etc/firmware/bcm943241ng1630_p252.txt iface_name=wlan0
+
+logFile=/data/bootCount.tt
+adv_hctosys_sleep_time=7200
+
+
+bootCount()
+{
+
+	if [ -e ${logFile} ] ; then 
+		logNu=`cat ${logFile}`
+		countNu=$((${logNu}+1))
+		echo ${countNu} > ${logFile}
+	else
+		touch ${logFile}
+		echo "1" > ${logFile}
+	fi
+}
+
+adv_hctosys()
+{
+	echo "adv_hctosys -- sync every " ${adv_hctosys_sleep_time} " seconds"
+
+	while :
+	do
+		sleep ${adv_hctosys_sleep_time}
+		busybox hwclock -s -u
+	done
+}
+
+init_dev()
+{
+	chmod 777 -R /data/local/tmp
+}
+
+init_dev
+bootCount
+#adv_hctosys
